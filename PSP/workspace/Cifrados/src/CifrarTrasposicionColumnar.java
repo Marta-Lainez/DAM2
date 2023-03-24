3c0a923c4d7b25b4d94bd8204a0cbce9147432bd
MartaLainez <aa21.mlainez83@iespabloserrano.com>
2023-03-24 20:36:58 +0100

    Añado PSP cifrado columnas

--- PSP/workspace/Cifrados/src/CifrarTrasposicionColumnar.java
+++ PSP/workspace/Cifrados/src/CifrarTrasposicionColumnar.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
 public class CifrarTrasposicionColumnar {
+	public static void main (String[]args) {
+		String frase = "Me gustan las papas";
+		System.out.println(frase);
+		System.out.println(cifrar(frase, 5));
+	}
 	// Me gustan las papas
 	/* MEGUS
 	 * TANLA
@@ -16,16 +21,32 @@ public class CifrarTrasposicionColumnar {
 		}
 		
 		String cifrado = "";
-		int numFilas = (int)Math.ceil(nuevaCadena.length() / columnas);
-		ArrayList <ArrayList> filas = new ArrayList<>(numFilas);
+		int numFilas = (int)Math.ceil((double)nuevaCadena.length() / columnas);
+		// Creo matriz
+		 Character[][] matriz = new Character[numFilas][columnas];
 		
-		//String CadenasPorColumnas = nuevaCadena.
+		 System.out.println("Columnas: " + columnas);
+		 System.out.println("Letras: " + nuevaCadena.length());
+		 System.out.println("Filas:" + numFilas);
+		 
+		// Recorremos la matriz
+		for(int i = 0; i < columnas; i++) {
+			int contador = 0;
+			for(int j = 0; j < numFilas; j++) {
+				char letra = nuevaCadena.charAt(contador);
+				matriz[i][j] = letra;
+				contador++;
+			}
+		}
+		// Sacamos la frase cifrada
 		for(int i = 0; i < numFilas; i++) {
-			for(int j = 0; i < nuevaCadena.length(); i++) {
-				
+			for(int j = 0; j < columnas; j++) {
+				Character letra = matriz[i][j];
+				if(letra != null)
+				cifrado += letra;
 			}
 		}
 		
-		return "";
+		return cifrado;
 	}
 }
